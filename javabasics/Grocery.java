e6f966a980375eddd03adf0e58d304bb27702edb
mdswaley <mdswaley075@gmail.com>
2026-09-10 10:17:28 +0530

    Grocery items from cmd args

---
+++ javabasics/Grocery.java
@@ -0,0 +1,11 @@
+package javabasics;
+
+public class Grocery {
+    public static void main(String[] args) {
+        String itemName = args[0];
+        String fressness = args[1];
+        Double price = Double.parseDouble(args[2]);
+
+        System.out.println("you ordered : "+itemName+" comming before : "+fressness+" price : "+price);
+    }
+}
