ad996c25dcf0a78fe0e95f611434f6fb570163c6
mdswaley <mdswaley075@gmail.com>
2026-09-10 09:57:02 +0530

    Booking car cmd args

---
+++ javabasics/BookingCar.java
@@ -0,0 +1,20 @@
+package javabasics;
+
+public class BookingCar {
+    public static void main(String[] args) {
+
+        if(args.length < 4){
+            System.out.println("Please Enter all details of car ");
+            return;
+        }
+
+        String carName = args[0];
+        String color = args[1];
+        double carPrice = Double.parseDouble(args[2]);
+        String modelName = args[3];
+
+
+        System.out.println("Booking "+carName+" with color "+color+" price : "+carPrice+" model name "+modelName);
+
+    }
+}
