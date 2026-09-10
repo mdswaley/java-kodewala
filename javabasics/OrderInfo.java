e8661b23cb5e9695ddce0c36021f654cea999047
mdswaley <mdswaley075@gmail.com>
2026-09-10 08:58:18 +0530

    print order info through cmd args

---
+++ javabasics/OrderInfo.java
@@ -0,0 +1,18 @@
+package javabasics;
+
+public class OrderInfo {
+    // wap order ID, product name, placed by, delivery address info from command line args
+
+    public static void main(String[] args) {
+        int orderId = Integer.parseInt(args[0]);
+        String productName = args[1];
+        String placedBy = args[2];
+        String deliveryAdd = args[3];
+
+        System.out.println(orderId);
+        System.out.println(productName);
+        System.out.println(placedBy);
+        System.out.println(deliveryAdd);
+
+    }
+}
