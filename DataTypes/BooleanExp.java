6aef9750f3ce328741a8c27c700310d752d6cfa7
mdswaley <mdswaley075@gmail.com>
2026-09-12 10:56:03 +0530

    greater age

--- DataTypes/BooleanExp.java
+++ DataTypes/BooleanExp.java
@@ -1,10 +1,18 @@
 package DataTypes;
 
 public class BooleanExp {
-    static boolean isValid;
+    static boolean isValid = false;
     public static void main(String[] args) {
 
         System.out.println("Default value for boolean is : "+isValid);
+
+        int age = 17;
+
+        if(age > 18){
+            isValid = true;
+        }
+
+        System.out.println(isValid);
         
     }
 }
