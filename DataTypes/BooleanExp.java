3c365ab94699ef9d037b26a2767664675e39aac2
mdswaley <mdswaley075@gmail.com>
2026-09-12 10:53:54 +0530

    Boolean example

---
+++ DataTypes/BooleanExp.java
@@ -0,0 +1,10 @@
+package DataTypes;
+
+public class BooleanExp {
+    static boolean isValid;
+    public static void main(String[] args) {
+
+        System.out.println("Default value for boolean is : "+isValid);
+        
+    }
+}
