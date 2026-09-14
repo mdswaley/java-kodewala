4fccacd09e3c329b96051b1812a2e8cafcd3e0b0
mdswaley <mdswaley075@gmail.com>
2026-09-14 19:10:25 +0530

    String examples

---
+++ DataTypes/StringExp.java
@@ -0,0 +1,10 @@
+package DataTypes;
+
+public class StringExp {
+    static String s;
+    public static void main(String[] args) {
+
+        System.out.println("Default value of String : "+s); // null
+        
+    }
+}
