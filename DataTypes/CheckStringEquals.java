8ded0e7ac998b28fa8f992b6a4931f7db30b5d21
mdswaley <mdswaley075@gmail.com>
2026-09-14 19:13:15 +0530

    check string equals or not

---
+++ DataTypes/CheckStringEquals.java
@@ -0,0 +1,14 @@
+package DataTypes;
+
+public class CheckStringEquals {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+
+        if(s1.equals(s2)){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+}
--- DataTypes/StringExp.java
+++ DataTypes/StringExp.java
@@ -3,8 +3,6 @@ package DataTypes;
 public class StringExp {
     static String s;
     public static void main(String[] args) {
-
         System.out.println("Default value of String : "+s); // null
-        
     }
 }
