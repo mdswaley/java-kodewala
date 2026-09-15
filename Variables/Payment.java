fa5b336ab502e783e88ddba59dd7f4ae7a2c1ef8
mdswaley <mdswaley075@gmail.com>
2026-09-15 08:50:18 +0530

    payment info

---
+++ Variables/Payment.java
@@ -0,0 +1,12 @@
+package Variables;
+
+public class Payment{
+    public static void main(String[] args) {
+        int balance = 1000;
+        int balanceDeduction = 100;
+        
+        int total = balance - balanceDeduction;
+
+        System.out.println(total);
+    }
+}
