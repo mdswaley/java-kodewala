a2ae73064c8b6aef73137a768bce9965effd560f
mdswaley <mdswaley075@gmail.com>
2026-09-15 09:21:20 +0530

    update payment class

--- Variables/Payment.java
+++ Variables/Payment.java
@@ -1,12 +1,21 @@
 package Variables;
 
 public class Payment{
+    static int total; // static variables
+
     public static void main(String[] args) {
         int balance = 1000;
         int balanceDeduction = 100;
         
-        int total = balance - balanceDeduction;
+        total = balance - balanceDeduction;
 
+        System.out.println("Total available balance : " + total); // Total available balance : ---> static 
+        // total ---> dynamic
+
+        m2();
+    }
+
+    static void m2(){
         System.out.println(total);
     }
 }
