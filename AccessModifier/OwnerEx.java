032da84bedf4dc147210e33dd21c59b281bf17f9
mdswaley <mdswaley075@gmail.com>
2026-09-18 20:18:47 +0530

    accessbility for inheritance

--- AccessModifier/OwnerEx.java
+++ AccessModifier/OwnerEx.java
@@ -7,9 +7,14 @@ public class OwnerEx {
 }
 
 class Client1{
-
+    public void display(){
+        System.out.println("Chient1 is called...");
+    }
 }
 
-class Client2{
-
+class Client2 extends Client1{
+    protected  void display(){ // if you decrease child class data accessebility it will give compile time error. like public -> protected, protected -> default etc
+        // But if you increase like protected -> public, default -> protected it will work.
+        System.out.println("Chient2 is called...");
+    }
 }
