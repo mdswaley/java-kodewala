4f1ec9a0aab44fbbc477d1444379e886601a24d0
mdswaley <mdswaley075@gmail.com>
2026-09-19 10:49:49 +0530

    explain

--- AccessModifier/OwnerEx.java
+++ AccessModifier/OwnerEx.java
@@ -13,7 +13,7 @@ class Client1{
 }
 
 class Client2 extends Client1{
-    protected  void display(){ // if you decrease child class data accessebility it will give compile time error. like public -> protected, protected -> default etc
+    public void display(){ // if you decrease child class data accessebility it will give compile time error. like public -> protected, protected -> default etc
         // But if you increase like protected -> public, default -> protected it will work.
         System.out.println("Chient2 is called...");
     }
