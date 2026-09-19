052da40a2732164bc19e43ac4c5bc4ce54f81ccb
mdswaley <mdswaley075@gmail.com>
2026-09-19 10:56:37 +0530

    making changes

--- BankingSystem/Account.java
+++ BankingSystem/Account.java
@@ -3,11 +3,21 @@ package BankingSystem;
 public class Account {
     public static void main(String[] args) {
         BankAccount a1 = new BankAccount(1200, 4352);
+        Transaction t1 = new Transaction();
 
         a1.deposit(2300);
         System.out.println(a1.getBalance());
+        
+        t1.printTransaction();
 
         a1.withdraw(530);
         System.out.println(a1.getBalance());
+
+        t1.printTransaction();
+
+        SavingsAccount s1 = new SavingsAccount(2000, 5312);
+        s1.checkBalanceRule();
+
+        t1.printTransaction();
     }
 }
--- BankingSystem/SavingsAccount.java
+++ BankingSystem/SavingsAccount.java
@@ -3,6 +3,7 @@ package BankingSystem;
 public class SavingsAccount extends BankAccount {
     public SavingsAccount(double balance, int pin) {
         super(balance, pin);
+        System.out.println("\nCreate saving account..");
     }
 
     public void checkBalanceRule(){
