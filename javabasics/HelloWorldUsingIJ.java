de13b8a45fdb70d141dc9d230bee9594d2ab0514
mdswaley <mdswaley075@gmail.com>
2026-09-21 09:50:57 +0530

    first code using intellij idea

---
+++ javabasics/HelloWorldUsingIJ.java
@@ -0,0 +1,10 @@
+package javabasics;
+import java.util.Scanner;
+
+public class HelloWorldUsingIJ {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello world from Intellij Idea...");
+    }
+}
