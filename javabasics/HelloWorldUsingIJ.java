055c5a2569ce9ae2391ccc9712e946f6242545a4
mdswaley <mdswaley075@gmail.com>
2026-09-21 10:07:48 +0530

    how to pass command line argument

--- javabasics/HelloWorldUsingIJ.java
+++ javabasics/HelloWorldUsingIJ.java
@@ -2,9 +2,13 @@ package javabasics;
 import java.util.Scanner;
 
 public class HelloWorldUsingIJ {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String s = args[0]; // run / debug configuration --> edit config --> program argument
+        System.out.println("Hello world from Intellij Idea..."+s);
 
-        System.out.println("Hello world from Intellij Idea...");
+        int amount = 100;
+
+        System.out.println("Amount : "+amount);
     }
 }
