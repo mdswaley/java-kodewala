3be814816d33c96f0a6f53ea101ef881bc7bd8d8
mdswaley <mdswaley075@gmail.com>
2026-09-25 17:55:34 +0530

    for each user create list of item with total price

--- Method/CalculateTotalPrice.java
+++ Method/CalculateTotalPrice.java
@@ -3,23 +3,49 @@ package Method;
 class Items{
     String itemName;
     double price;
-    static double total;
 
     Items(String itemName, double price){
         this.itemName = itemName;
         this.price = price;
-        total += this.price;
+    }
+}
+
+class User {
+    String userName;
+    int count;
+    Items[] items;
+
+    User(String userName, int size){
+        this.userName = userName;
+        this.items = new Items[size];
+    }
+
+    void addItems(Items item){
+        items[count] = item;
+        count++;
+    }
+
+    double totalPrice(){
+        double total = 0;
+
+        for(int i=0;i<count;i++){
+            total += items[i].price;
+        }
+
+        return total;
     }
 }
 
 public class CalculateTotalPrice {
     public static void main(String[] args) {
-        Items i1 = new Items("TShirt", 530);
-        Items i2 = new Items("Jeans", 1200);
-        Items i3 = new Items("Shoes", 2000);
 
+        User u1 = new User("Swaley", 3);
 
-        System.out.println("Total price : "+Items.total);
+        u1.addItems(new Items("TShirt", 530));
+        u1.addItems(new Items("Jeans", 1200));
+        u1.addItems(new Items("Shoes", 2000));
+
+        System.out.println("Total price for user : "+u1.userName+" is : "+u1.totalPrice());
 
     }
 
